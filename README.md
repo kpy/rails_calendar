40c3a1f3560d26bae133d1dcc33ec40b4ccc2eab
kpy <kpy@web.de>
2012-09-17 17:01:58 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Einfacher Ruby on Rails Kalender
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsCalendar::Application.config.secret_token = 'bba8102865adb24e91c1f7b5da9141cc8cb14d7e32b1657d6388b261c0a1b9696555d5ac18118f8c9bfafea35ad704a21081e161b5d04b2e2ec8ec3b72f21e5e'
