9c6bd64048eea79016896ca8df01e5eda18fa7be
kpy <kpy@web.de>
2012-09-18 17:31:05 +0200

    Arbeiten an User model abgeschlossen

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,38 +13,20 @@
 
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
+  has_secure_password
 
-  attr_accessor :password
-  
-  before_save :encrypt_password
   before_save { |user| user.email = email.downcase }
-
-  validates_confirmation_of :password
-  validates_presence_of :password, :on => :create
 
   # Länge des Benutzernamens auf 50 Zeichen beschränken
   validates :name,  presence: true, length: { maximum: 50 }
-
+  
   # Mit regulärem Ausdruck aussehen der Email-Adresse festlegen
   # Email-Adresse muss einmalig sein
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, 
             uniqueness: { case_sensitive: false }
 
-
-  def self.authenticate(email, password)
-	  user = User.find_by_email(email)
-	  if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
-	    user
-	  else
-	    nil
-	  end
-  end 
-
-  def encrypt_password
-	  if password.presence?
-	    self.password_salt = BCrypt::Engine.generate_salt
-	    self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
-	  end
-  end
+  # Passwort muss mindestens 6 Zeichen haben
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
 end
